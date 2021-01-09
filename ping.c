fe2009ba914597826ecf3b3f641aa3bbce64bfd8
DorBaram <dorbaram@gmail.com>
2021-01-09 21:39:02 +0200

    added times

--- ping.c
+++ ping.c
@@ -5,17 +5,18 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <netinet/ip_icmp.h>
+#include <sys/time.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define IPAddress "127.0.0.1"
 #define PACKETSIZE	64
 struct packet
 {
 	struct icmphdr hdr;
-	char msg[PACKETSIZE-sizeof(struct icmphdr)];
+	char msg[1];
 };
-
-int pid=-1;
-struct protoent *proto=NULL;
 
 /*--------------------------------------------------------------------*/
 /*--- checksum - standard 1s complement checksum                   ---*/
@@ -39,45 +40,45 @@ unsigned short checksum(void *b, int len)
 /*--- ping - Create message and send it.                           ---*/
 /*--------------------------------------------------------------------*/
 void ping(struct sockaddr_in *addr)
-{	const int val=255;
-	int i, sd, cnt=1;
-	struct packet pckt;
-	struct sockaddr_in r_addr;
+{	
+	struct timeval stop_m, start_m;
 
-	sd = socket(PF_INET, SOCK_RAW, proto->p_proto);
-	if ( sd < 0 )
+	struct packet paketa;
+	struct sockaddr_in address;
+
+	int sock = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
+	if ( sock < 0 )
 	{
 		perror("socket");
 		return;
 	}
 
-	if ( fcntl(sd, F_SETFL, O_NONBLOCK) != 0 )
+	if ( fcntl(sock, F_SETFL, O_NONBLOCK) != 0 )
 		perror("Request nonblocking I/O");
 		int bool = 1;
 	while(bool)
 	{
-		int len=sizeof(r_addr);
-
-		bzero(&pckt, sizeof(pckt));
-		pckt.hdr.type = ICMP_ECHO;
-		pckt.hdr.un.echo.id = pid;
-		for ( i = 0; i < sizeof(pckt.msg)-1; i++ )
-			pckt.msg[i] = i+'0';
-		pckt.msg[i] = 0;
-		pckt.hdr.un.echo.sequence = cnt++;
-		pckt.hdr.checksum = checksum(&pckt, sizeof(pckt));
-		if ( sendto(sd, &pckt, sizeof(pckt), 0, (struct sockaddr*)addr, sizeof(*addr)) <= 0 )
+		
+		bzero(&paketa, sizeof(paketa));
+		paketa.hdr.type = ICMP_ECHO;
+		paketa.hdr.un.echo.id = 27;																	//some lucky number for the ID
+		paketa.hdr.checksum = checksum(&paketa, sizeof(paketa));
+		gettimeofday(&start_m, NULL);
+		if ( sendto(sock, &paketa, sizeof(paketa), 0, (struct sockaddr*)addr, sizeof(*addr)) <= 0 )
 			perror("sendto");
-
 		printf("Ping\n");
-		if ( recvfrom(sd, &pckt, sizeof(pckt), 0, (struct sockaddr*)&r_addr, &len) > 0 ){
+		int addressLength=sizeof(address);
+		if ( recvfrom(sock, &paketa, sizeof(paketa), 0, (struct sockaddr*)&address, &addressLength) > 0 ){
+			gettimeofday(&stop_m, NULL);
 			printf("Pong\n");
 			bool = 0;
+			printf("RTT time in microseconds is: %ld\n", (stop_m.tv_sec - start_m.tv_sec)* 1000000 +(stop_m.tv_usec - start_m.tv_usec));
+	    	printf("RTT time in milliseconds is: %ld\n", ((stop_m.tv_sec - start_m.tv_sec)* 1000000 +(stop_m.tv_usec - start_m.tv_usec))/1000);
 		}
+		
 
 	}
 }
-
 
 /*--------------------------------------------------------------------*/
 /*--- main - look up host and start ping processes.                ---*/
@@ -86,14 +87,13 @@ int main()
 {	struct hostent *hname;
 	struct sockaddr_in addr;
 
-		pid = getpid();
-		proto = getprotobyname("ICMP");
 		hname = gethostbyname(IPAddress);
 		bzero(&addr, sizeof(addr));
 		addr.sin_family = hname->h_addrtype;
 		addr.sin_port = 0;
 		addr.sin_addr.s_addr = *(long*)hname->h_addr;
 		ping(&addr);
+		
 		wait(0);
 	
 	return 0;
